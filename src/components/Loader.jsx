d3baaf28aa108da6433641d39dd27674b13bcb00
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-04-16 01:54:17 +0100

    removed redundant codes.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 
 function Loader() {
-  square.register();
+
   return (
     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center ">
      <p>Loading Please wait...</p>
